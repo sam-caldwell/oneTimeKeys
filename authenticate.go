b993c72defcdc5c9d32986ba9eeb23ba95082f74
Sam Caldwell <github@samcaldwell.net>
2024-08-10 16:40:50 -0500

    fix typo in authenticate

--- authenticate.go
+++ authenticate.go
@@ -1,10 +1,8 @@
 package oneTimeKeys
 
-import "bytes"
-
 // Authenticate - verify a given claimed key exists in the GenericQueue and delete it if so before returning true.
 // otherwise return false and do nothing else.
 func Authenticate(claimedKey OneTimeKey) (ok bool) {
-	ok = otkQueue.DeleteIfExists(claimedKey, bytes.Compare)
+	ok = otkQueue.DeleteIfExists(claimedKey)
 	return ok
 }
